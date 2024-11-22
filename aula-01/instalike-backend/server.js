260a38b662fbfd305b55da558ba2c554910f7d7f
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-11-21 21:08:30 -0300

    Aula 02

--- aula-01/instalike-backend/server.js
+++ aula-01/instalike-backend/server.js
@@ -1,12 +1,53 @@
-// Aula 01 | Imersão Back-end Alura | Aluno: Lucas E. Dickmann
+// Imersão Back-end Alura | Aluno: Lucas E. Dickmann
 
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "Um gatinho astronauta explorando um novo planeta",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Um gatinho comandante dando ordens à sua tripulação de ratinhos",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 3,
+    descricao: "Um gatinho astronauta fazendo um selfie em um buraco negro",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 4,
+    descricao: "Um gatinho astronauta cultivando plantas em uma estação espacial",
+    image: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 5,
+    descricao: "Um gatinho astronauta pilotando um disco voador",
+    image: "https://placecats.com/millie/300/150",
+  }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Servidor escutando ");
 });
 
-app.get("/api", (rep, res) => {
-  res.status(200).send("Bem vindo a API do Instalike | Imersão Back-end Alura");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  })
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
 });
